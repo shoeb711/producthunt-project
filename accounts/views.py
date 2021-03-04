118a31538b47b1b7cdd21694e491575f5eafd9ed
shoeb <shaikhshoeb772@gmail.com>
2021-03-04 20:11:29 +0530

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -5,13 +5,13 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
-        if request.POST['passowrd1'] == request.POST['password2']:
+        if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error': 'Username Already Exist'})
             except User.DoesNotExist:
-                user = User.objects.create_user(request, user)
-                auth.login(request)
+                user = User.objects.create_user(request.POST['username'], request.POST['password1'])
+                auth.login(request, user)
                 return redirect('home')
         else:
             return render(request, 'accounts/signup.html', {'error': 'Password Does Not Match'})
